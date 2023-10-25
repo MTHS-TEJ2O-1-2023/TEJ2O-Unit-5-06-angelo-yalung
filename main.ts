202969b2f0f30ffc2f7cb03044e877b32dff034e
angelo-yalung <144264086+angelo-yalung@users.noreply.github.com>
2023-10-25 12:15:46 -0400

    asg

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Mr. Angelo Yalung
+ * Created on: Oct 2023
+ * This program tells the user the distance between the microbit and the corner of the room using a sonar
 */
 
 basic.showString('Hello, World!')
